e5238f971636624619fae9be58569fc8f8747627
Shaney Waris <shaney18308@iiitd.ac.in>
2021-04-10 06:53:27 +0530

    updates!

--- main1.py
+++ main1.py
@@ -1,7 +1,7 @@
 from num2words import num2words
 import nltk
-# nltk.download('brown')
-# nltk.download('names')
+nltk.download('brown')
+nltk.download('names')
 nltk.download('stopwords')
 from nltk.util import ngrams
 from nltk.corpus import stopwords
